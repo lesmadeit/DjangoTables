702b31846a0cbe79833ccc3671d5dbe78cbf8b0a
Leslie <lesmwendwa@gmail.com>
2026-05-13 12:40:54 +0300

    git ignore

--- DjangoTables/DjangoTables/settings.py
+++ DjangoTables/DjangoTables/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4zqlapxu2=*do#zqztf8g_6zc)u8@$5q!787$g&oe95ecq+a9x'
+
+SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
